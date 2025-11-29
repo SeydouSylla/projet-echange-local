6e2b8557510b6998fd9e3e8572b82c5681c06074
Seydou Sylla <seydousylla180@gmail.com>
2025-11-29 21:35:21 +0100

    Initialisation du projet Spring Boot avec architecture en couches

---
+++ src/main/java/echangelocal/ProjetEchangeLocalApplication.java
@@ -0,0 +1,13 @@
+package echangelocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetEchangeLocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetEchangeLocalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/echangelocal/ProjetEchangeLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package echangelocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetEchangeLocalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
